c53f25bb2a3a409f908f4dd42c2504227ffc36d4
GeraSaucedo <jegsg@hotmail.es>
2022-08-10 16:06:10 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:push_notif_test/screens/home_screen.dart';
+import 'package:push_notif_test/screens/message_screen.dart';
+import 'package:push_notif_test/services/push_notifications_service.dart';
+
+void main() async { //Transform main to async
+
+    /// First make sure widgets was initialized
+    /// If don't do this, notification service won't 
+    /// work cause widgets need to be initialized
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Initialize push notification service 
+    await PushNotificationService.initializeApp();
+    
+
+    runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+  
+
+  @override
+  void initState() {
+    super.initState();
+
+    //suscribe to notification message stream 
+    PushNotificationService.messageStream.listen((message) {
+        
+        // Navigate to new page
+        navigatorKey.currentState?.pushNamed('message', arguments: message);
+        
+        // show snackbar 
+        scaffoldMessengerKey.currentState?.showSnackBar( SnackBar(content: Text(message)));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      navigatorKey: navigatorKey, //Navigation
+      scaffoldMessengerKey: scaffoldMessengerKey, //Snackbars
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomeScreen(),
+        'message': (_) => const MessageScreen(),
+      },
+    );
+  }
+}
---
+++ lib/services/push_notifications_service.dart
@@ -0,0 +1,68 @@
+//SHA1 - DB:19:85:52:57:0F:80:D7:CD:BA:2C:7A:8D:E2:1A:9F:A8:00:6A:9B
+
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationService {
+
+  //Crate tihe firebase messaging instance
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+  static String? token;
+
+  /// Allow  emit a value in order to suscribe to notification messages
+  static final StreamController<String> _messageStream = StreamController.broadcast();
+
+  //Expose the stream 
+   static Stream<String> get messageStream => _messageStream.stream;
+
+  /// handle the recived [message] notifications  when the device is on background
+  static Future _onBackgroundHandler(RemoteMessage message ) async {
+      
+      // send to the message stream the data we going to use on the stream  
+      _messageStream.add(message.data['producto'] ?? 'No data');
+  }
+
+   /// handle the recived [message] notifications  when app is running
+  static Future _onMessageHandler(RemoteMessage message ) async {
+      
+       // send to the message stream the data we going to use on the stream  
+      _messageStream.add(message.data['producto'] ?? 'No data');
+  }
+
+   /// handle the recived [message] notifications  when notification was opened
+  static Future _onMessageOpenedHandler(RemoteMessage message ) async {
+
+       // send to the message stream the data we going to use on the stream  
+      _messageStream.add(message.data['producto'] ?? 'No data');
+  }
+
+  static Future initializeApp() async {
+
+      //Push notifications ---------------------------------------
+    //Make sure firebase is initialized
+     await Firebase.initializeApp(); 
+     
+     //Get the device token we need to send push notifications
+     //you have to save this token somewhere
+     token = await FirebaseMessaging.instance.getToken(); 
+
+     //handlers
+     FirebaseMessaging.onBackgroundMessage(_onBackgroundHandler);
+     FirebaseMessaging.onMessage.listen(_onMessageHandler);
+     FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenedHandler);
+
+     print(token);
+
+      //Local notifications
+
+  }
+
+  /// is a good practice close streams to save memory, in this case we dont close the stream
+  /// because is necessary be alive all time
+  static closeStreams() {
+    _messageStream.close();
+  }
+
+}
